effc69a38f662abbaf90690bc1b70fa7678d67ac
Ella van Durpe <ella@vandurpe.com>
2020-09-16 09:50:50 +0300

    Add eslint warn rule

--- packages/eslint-plugin/configs/custom.js
+++ packages/eslint-plugin/configs/custom.js
@@ -4,12 +4,19 @@ module.exports = {
 		'@wordpress/no-unused-vars-before-return': 'error',
 		'@wordpress/no-base-control-with-label-without-id': 'error',
 		'@wordpress/no-unguarded-get-range-at': 'error',
+		'@wordpress/no-global-active-element': 'warn',
 	},
 	overrides: [
 		{
 			files: [ '*.native.js' ],
 			rules: {
 				'@wordpress/no-base-control-with-label-without-id': 'off',
+			},
+		},
+		{
+			files: [ '*.test.js', '**/test/*.js' ],
+			rules: {
+				'@wordpress/no-global-active-element': 'off',
 			},
 		},
 	],
